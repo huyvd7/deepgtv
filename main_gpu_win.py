10656626dd8dff1d1ee6e47f6b4cbee6fe1f34dd
Huy D. Vu <huyvd7@gmail.com>
2020-03-13 21:17:57 -0400

    Update main_gpu_win.py

--- main_gpu_win.py
+++ main_gpu_win.py
@@ -416,8 +416,7 @@ class GTV(nn.Module):
         if debug:
             hist = [h.flatten() for h in hist]
             return hist
-        # xhat = _norm(xhat, 0, 255)
-        print(xhat.min(), xhat.max())
+        xhat = _norm(xhat, 0, 255)
         return xhat.view(xhat.shape[0], xhat.shape[1], opt.width, opt.width)
     
     def predict(self, xf):
