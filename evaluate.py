65a69f47e802c3c2c92a44a3fc4507f9817b1f45
Huy D. Vu <huyvd7@gmail.com>
2020-03-30 15:31:42 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -244,4 +244,4 @@ def main_eva(seed, model_name, trainset, testset, imgw=324, verbose=0):
 if __name__=="__main__":
     global opt
     supporting_matrix(opt)
-    main_eva(seed='_', model_name='GTV_20.pkl', trainset=['10','1','7','8','9'], testset=['2','3','4','5','6'],width=324, verbose=1)
+    main_eva(seed='_', model_name='GTV_20.pkl', trainset=['10','1','7','8','9'], testset=['2','3','4','5','6'],imgw=324, verbose=1)
