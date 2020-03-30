55168daadc1ad515b011518e622251cfec0946bf
Huy D. Vu <huyvd7@gmail.com>
2020-03-29 20:04:53 -0400

    Update main_gpu_win.py

--- main_gpu_win.py
+++ main_gpu_win.py
@@ -698,7 +698,7 @@ def main(seed, model_name, optim_name, subset=None, epoch=100, opt=None):
     dataset = RENOIR_Dataset(
         # img_dir=os.path.join('C:\\Users\\HUYVU\\AppData\\Local\\Packages\\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\\LocalState\\rootfs\\home\\huyvu\\dgtv_fullsize\\train'),
         img_dir=os.path.join(
-            "C:\\Users\\HUYVU\\AppData\\Local\\Packages\\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\\LocalState\\rootfs\\home\\huyvu\\dgtv\\train"
+            "C:\\Users\\HUYVU\\AppData\\Local\\Packages\\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\\LocalState\\rootfs\\home\\huyvu\\dgtv\\full"
         ),
         # img_dir=os.path.join('./train'),
         transform=transforms.Compose([standardize(normalize=False), ToTensor()]),
