41fb459a298212a532130c9a13ea6ef1ef2e9dc9
Huy D. Vu <huyvd7@gmail.com>
2020-04-01 13:45:35 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -746,7 +746,7 @@ def main(seed, model_name, optim_name=None, subset=None, epoch=100):
             optimizer.zero_grad()
             # forward + backward + optimize
             outputs = gtv(inputs, debug=0)
-            loss = criterion(outputs, labels)
+            #loss = criterion(outputs, labels)
             loss = ((labels-inputs)**2).mean()
             #print(loss, ((labels - inputs)**2).mean(axis=0).mean())
             loss.backward()
