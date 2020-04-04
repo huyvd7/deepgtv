ff8a311a9c77ad4a05ac41e97885f00d2f024278
Huy D. Vu <huyvd7@gmail.com>
2020-04-04 02:26:34 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -142,6 +142,7 @@ def denoise(inp, gtv, argref, normalize=False, stride=36, width=324, prefix='_',
     plt.imsave(opath, d)
     if argref:
         d = cv2.imread(opath)
+        d = cv2.cvtColor(d, cv2.COLOR_BGR2RGB)
         (score, diff) = compare_ssim(tref, d, full=True, multichannel=True)
         psnr2 = cv2.PSNR(tref, d)
         print("SSIM: {:.5f}".format(score))
