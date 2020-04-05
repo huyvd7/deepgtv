3d3aa316e473cb9add2462ca309efbf53cdc0cc0
Huy D. Vu <huyvd7@gmail.com>
2020-04-05 04:25:48 -0400

    update optim

--- experiment.py
+++ experiment.py
@@ -5,24 +5,14 @@ from main_gpu_artificial import *
 
 _subset = ['10', '1', '2', '3', '4', '5', '6', '7','8','9']
 model_name = 'GTV.pkl'
-opt = OPT(
-    batch_size=50,
-    admm_iter=4,
-    prox_iter=3,
-    delta=0.1,
-    channels=3,
-    eta=0.3,
-    u=25,
-    lr=1e-5,
-    momentum=0.9,
-    u_max=75,
-    u_min=50,
-)
+
+opt = OPT(batch_size = 50, admm_iter=4, prox_iter=3, delta=.1, channels=3, eta=.3, u=25, lr=5e-6, momentum=0.9, u_max=65, u_min=55)
+
 result = dict({'psnr_train':list(), 'ssim_train':list(),
                 'psnr_test':list(), 'ssim_test':list(),
                 'mse_train':list(), 'mse_test':list()})
 
-for i in range(1, 6):
+for i in range(1, 3):
     random.seed(i)
     subset = random.sample(_subset, 5)
     testset = [i for i in _subset if i not in subset]
@@ -31,7 +21,7 @@ for i in range(1, 6):
     m = '{0}_{1}'.format(i, model_name)
     o = m + 'optim'
     cont=None
-    main(seed=i, model_name=m, cont=cont, epoch=200, subset=subset)
+    main(seed=i, model_name=m, cont=cont, epoch=400, subset=subset)
     #traineva, testeva = main_eva(seed=i, model_name=m, trainset=subset, testset=testset)
     traineva, testeva = main_eva(seed=i, model_name=m, trainset=subset, testset=testset, verbose=1, image_path='..\\gauss', noise_type='gauss')
     result['psnr_train'].append(traineva['psnr2'])
--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -788,13 +788,13 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
             print("\t", np.argmin(histW), min(histW), histW)
 
         #scheduler.step() 
-        if (epoch+1) in [80, 120]:
+        if (epoch+1) in [80, 200, 350]:
             print("CHANGE LR")
             current_lr /= 5
-            optimizer = optim.SGD(gtv.parameters(), lr=current_lr, momentum=opt.momentum)
-#            optimizer = optim.SGD([
-#                    {'params': base_params},
-#                    {'params': cnny_params , 'lr': current_lr*50}], lr=current_lr, momentum=opt.momentum)
+            #optimizer = optim.SGD(gtv.parameters(), lr=current_lr, momentum=opt.momentum)
+            optimizer = optim.SGD([
+                    {'params': base_params},
+                    {'params': cnny_params , 'lr': current_lr*50}], lr=current_lr, momentum=opt.momentum)
     torch.save(gtv.state_dict(), SAVEPATH)
     torch.save(optimizer.state_dict(), SAVEPATH + "optim")
     print("Total running time: {0:.3f}".format(time.time() - tstart))
@@ -821,4 +821,4 @@ if __name__=="__main__":
     else:
         cont = None
 
-    main(seed=1, model_name='GTV.pkl', cont=cont, epoch=200, subset=['1', '3', '5', '7', '9'])
+    main(seed=1, model_name='GTV.pkl', cont=cont, epoch=400, subset=['1', '3', '5', '7', '9'])
