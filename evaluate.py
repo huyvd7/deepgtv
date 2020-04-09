6883a399b2e1a6580bc219fc937b1823014a794f
Huy D. Vu <huyvd7@gmail.com>
2020-04-09 17:38:53 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -300,4 +300,4 @@ if __name__=="__main__":
     channels=int(args.channels)
     opt.channels=channels
     
-    _, _ = main_eva(seed='gauss', model_name=model_name, trainset=['1', '3', '5', '7', '9'], testset=['10', '2', '4', '6', '8'],imgw=imgw, verbose=1, image_path=image_path, noise_type='gauss')
+    _, _ = main_eva(seed='gauss', model_name=args.model, trainset=['1', '3', '5', '7', '9'], testset=['10', '2', '4', '6', '8'],imgw=imgw, verbose=1, image_path=image_path, noise_type='gauss')
