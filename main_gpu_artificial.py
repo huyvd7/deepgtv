54e7c895555e576045652544f1f05a5bcf488986
Huy D. Vu <huyvd7@gmail.com>
2020-04-09 20:24:12 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -774,7 +774,7 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
     ax.plot(ma_vec)
     fig.savefig("loss.png")
 
-opt = OPT(batch_size = 50, admm_iter=4, prox_iter=3, delta=.1, channels=1, eta=.3, u=25, lr=8e-6, momentum=0.9, u_max=100, u_min=1)
+opt = OPT(batch_size = 50, admm_iter=4, prox_iter=3, delta=.1, channels=3, eta=.3, u=25, lr=8e-6, momentum=0.9, u_max=100, u_min=1)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
