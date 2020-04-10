be9bbeaaa53ec7e9fca8c2b59fc696e61b4a2197
Huy D. Vu <huyvd7@gmail.com>
2020-04-10 17:04:33 -0400

    different grad clip

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -738,11 +738,11 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
             # forward + backward + optimize
             outputs = gtv(inputs, debug=0)
             loss = criterion(outputs, labels)
-            #loss = ((labels-inputs)**2).mean()
-            #out = loss(x, t).sum() / batch_size
-            #print(loss, ((labels - outputs)**2).mean(axis=0).mean())
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(gtv.parameters(), 1e5)
+            #torch.nn.utils.clip_grad_norm_(gtv.parameters(), 1e5)
+            torch.nn.utils.clip_grad_norm_(cnnf_params, 1e5)
+            torch.nn.utils.clip_grad_norm_(cnny_params, 1e5)
+            torch.nn.utils.clip_grad_norm_(cnnu_params, 1e2)
 
             optimizer.step()
             running_loss += loss.item()
