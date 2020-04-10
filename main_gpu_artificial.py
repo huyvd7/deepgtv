8b50065400f194a81ac57c6d42fc4db09e05d24b
Huy D. Vu <huyvd7@gmail.com>
2020-04-10 18:04:23 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -711,9 +711,9 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
     cnnu_params = list(filter(lambda kv: 'cnnu' in kv[0], gtv.named_parameters()))
     cnnu_params = [i[1] for i in cnnu_params ]
     optimizer = optim.SGD([
-                {'params': cnny_params, 'lr':opt.lr*1.2},
+                {'params': cnny_params, 'lr':opt.lr*2},
                  {'params': cnnf_params , 'lr': opt.lr*50},
-                 {'params': cnnu_params , 'lr': opt.lr*4}
+                 {'params': cnnu_params , 'lr': opt.lr*3.5}
              ], lr=opt.lr, momentum=opt.momentum)
     #optimizer = optim.SGD(gtv.parameters(), lr=opt.lr, momentum=opt.momentum)
     if cont:
