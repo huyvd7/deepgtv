57b6b70ba926e1c065cd190c5feaeeccdf04505f
Huy D. Vu <huyvd7@gmail.com>
2020-04-10 22:20:50 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -818,5 +818,5 @@ if __name__=="__main__":
     else:
         model_name='GTV.pkl'
     opt.batch_size = int(args.batch) 
-    opt.lr = int(args.lr)
+    opt.lr = float(args.lr)
     main(seed=1, model_name=model_name, cont=cont, epoch=200, subset=['1', '3', '5', '7', '9'])
