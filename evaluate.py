7d3f947ba2c28090a10db14c1879f336530b8990
Huy D. Vu <huyvd7@gmail.com>
2020-04-16 03:00:24 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -183,6 +183,7 @@ def main_eva(seed, model_name, trainset, testset, imgw=None, verbose=0, image_pa
     # INITIALIZE
     global opt
     supporting_matrix(opt)
+    opt._print()
     gtv = GTV(
         width=36,
         prox_iter=1,
@@ -299,5 +300,5 @@ if __name__=="__main__":
         image_path = args.image_path
     else:
         image_path = '..\\gauss'
-    opt.delta = float(arg.delta)
+    opt.delta = float(args.delta)
     _, _ = main_eva(seed='gauss', model_name=model_name, trainset=['1', '3', '5', '7', '9'], testset=['10', '2', '4', '6', '8'],imgw=imgw, verbose=1, image_path=image_path, noise_type='gauss', Tmod=int(args.Tmod))
