fc2a09f6e73496a635484c4b52a5f1132b41a8be
Huy D. Vu <huyvd7@gmail.com>
2020-06-20 16:47:56 -0400

    Update dgtv_train.py

--- dgtv_train.py
+++ dgtv_train.py
@@ -121,9 +121,9 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
             outputs = gtv(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(cnnf_params, 1e2)
+            torch.nn.utils.clip_grad_norm_(cnnf_params, 2)
             torch.nn.utils.clip_grad_norm_(cnny_params, 1)
-            torch.nn.utils.clip_grad_norm_(cnnu_params, 1e2)
+            torch.nn.utils.clip_grad_norm_(cnnu_params, 2)
 
             optimizer.step()
             #optimizer[i%3].step()
