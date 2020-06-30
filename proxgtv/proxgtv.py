a7725e8f1e8cf34a15b132bb95331449e41a3b35
Huy D. Vu <huyvd7@gmail.com>
2020-06-30 01:49:36 -0400

    Update proxgtv.py

--- proxgtv/proxgtv.py
+++ proxgtv/proxgtv.py
@@ -30,8 +30,8 @@ class cnnf_2(nn.Module):
         #self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
         #x = x.view(-1, 16 * 5 * 5)
         #x = F.relu(self.fc1(x))
         #x = F.relu(self.fc2(x))
