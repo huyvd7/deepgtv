630bbf704ede4856e9807ab984c275361584439f
Huy D. Vu <huyvd7@gmail.com>
2020-07-05 18:12:39 -0400

    Update proxgtv.py

--- proxgtv/proxgtv.py
+++ proxgtv/proxgtv.py
@@ -567,11 +567,10 @@ class GTV(nn.Module):
 
         # REPLACE WITH MLP
         lagrange = self.opt.lagrange.requires_grad_(True)
-        print(w.shape)
         #########################
-        lagrange1 = self.mlp1(w)
-        lagrange2 = self.mlp2(w)
-        lagrange3 = self.mlp3(w)
+        lagrange1 = self.mlp1(w.view(w.shape[0], w.shape[1])
+        lagrange2 = self.mlp2(w.view(w.shape[0], w.shape[1])
+        lagrange3 = self.mlp3(w.view(w.shape[0], w.shape[1])
         ###################
 
 
