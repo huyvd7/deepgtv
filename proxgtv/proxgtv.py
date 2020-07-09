005d5d426c46e9b4c1579a6e8015c32c8fe5ff05
Huy D. Vu <huyvd7@gmail.com>
2020-07-09 01:44:32 -0400

    Update proxgtv.py

--- proxgtv/proxgtv.py
+++ proxgtv/proxgtv.py
@@ -27,7 +27,6 @@ class cnnf_2(nn.Module):
         self.layer = nn.Sequential(
             nn.Conv2d(opt.channels, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True),
             #nn.LeakyReLU(0.05),
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
