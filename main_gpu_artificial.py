68f5632fcfabe60d33e8abac8b545304014a1f5d
Huy D. Vu <huyvd7@gmail.com>
2020-07-24 19:14:14 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -163,7 +163,7 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
 
         if ((epoch + 1) % 1 == 0) or (epoch + 1) == total_epoch:
             with torch.no_grad():
-                histW = gtv(inputs[:1, :, :, :], debug=1, Tmod=opt.admm_iter + 5)
+                histW = gtv(inputs, debug=1, Tmod=opt.admm_iter + 5)
             if opt.ver: # experimental version
                 print("\tCNNF stats: ", gtv.cnnf.layer[0].weight.grad.mean())
             else:
