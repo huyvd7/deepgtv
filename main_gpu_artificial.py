d5cb201e06ecbbf164f139f02f4e968c8c3dfde3
Huy D. Vu <huyvd7@gmail.com>
2020-08-01 03:51:55 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -203,6 +203,7 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
     window_width = 30
     ma_vec = (cumsum_vec[window_width:] - cumsum_vec[:-window_width]) / window_width
     ax.plot(ma_vec)
+    ax.set(ylim=[0, ax.get_ylim()[1] *1.05 ])
     fig.savefig("loss.png")
 
 opt = OPT(batch_size = 50, admm_iter=4, prox_iter=3, delta=.1, channels=3, eta=.05, u=50, lr=8e-6, momentum=0.9, u_max=65, u_min=50)
