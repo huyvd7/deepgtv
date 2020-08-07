7162eb66ef558ded50a763592d55f0238144f51b
Huy D. Vu <huyvd7@gmail.com>
2020-08-07 03:18:40 -0400

    Update dgtv_train.py

--- dgtv_train.py
+++ dgtv_train.py
@@ -50,7 +50,7 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
         subset=subset,
     )
     dataloader = DataLoader(
-        dataset, batch_size=batch_size, shuffle=True  # , pin_memory=True
+        dataset, batch_size=batch_size, shuffle=True   , pin_memory=True, num_workers=4, drop_last=True
     )
 
     width = 36
