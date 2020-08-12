4e030631e26e113e886093986152ae7afa2c5cc2
Huy D. Vu <huyvd7@gmail.com>
2020-08-12 00:19:06 -0400

    Update dgtv_train.py

--- dgtv_train.py
+++ dgtv_train.py
@@ -79,17 +79,16 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
         gtv.cuda()
     criterion = nn.MSELoss()
     
-    cnny_params = list(filter(lambda kv: 'cnny' in kv[0] , gtv.named_parameters()))
-    cnny_params = [i[1] for i in cnny_params]
-    cnnf_params = list(filter(lambda kv: 'cnnf' in kv[0], gtv.named_parameters()))
-    cnnf_params = [i[1] for i in cnnf_params]
-    cnnu_params = list(filter(lambda kv: 'cnnu' in kv[0], gtv.named_parameters()))
-    cnnu_params = [i[1] for i in cnnu_params ]
+    gtv1_params = list(filter(lambda kv: 'gtv1' in kv[0] , gtv.named_parameters()))
+    gtv1_params = [i[1] for i in gtv1_params ]
+    gtv2_params = list(filter(lambda kv: 'gtv2' in kv[0], gtv.named_parameters()))
+    gtv2_params = [i[1] for i in gtv2_params]
+
 
     print(len(cnny_params))
     optimizer = optim.SGD([
-                {'params': cnny_params, 'lr':opt.lr},
-                 {'params': cnnf_params , 'lr': opt.lr*50}
+                {'params': gtv2_params, 'lr':opt.lr},
+                 {'params': gtv1_params , 'lr': opt.lr*50}
              ], lr=opt.lr, momentum=opt.momentum)
 
     #optimizer = optim.SGD(gtv.parameters(), lr=opt.lr, momentum=opt.momentum)
