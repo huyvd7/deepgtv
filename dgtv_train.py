bbd88ff117c509000e6589ee16fd4541219199c5
Huy D. Vu <huyvd7@gmail.com>
2020-09-25 01:11:52 -0400

    Update dgtv_train.py

--- dgtv_train.py
+++ dgtv_train.py
@@ -109,6 +109,7 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
     hist = list()
     losshist = list()
     tstart = time.time()
+    tprev=tstart
     opt._print()
     pickle.dump(opt, open( "dopt", "wb" ))
     ld = len(dataset)
@@ -154,13 +155,14 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
                     P3 = g(P2, debug=1, Tmod=opt.admm_iter + 5)
 
 
-
+        tnow = time.time()
         print(
             time.ctime(),
-            '[{0}] \x1b[31mLOSS\x1b[0m: {1:.3f}, time elapsed: {2:.1f} secs'.format(
-                epoch + 1, running_loss / (ld), time.time() - tstart
+            '[{0}] \x1b[31mLOSS\x1b[0m: {1:.3f}, time elapsed: {2:.1f} secs, epoch time: {2:.1f} secs'.format(
+                epoch + 1, running_loss / (ld), tnow - tstart, tnow-tprev
             )
         )
+        tprev=tnow
         
 
         if ((epoch + 1) % 1 == 0) or (epoch + 1) == total_epoch:
