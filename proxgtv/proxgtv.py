28bf832ab43d3cd902f19611930481bb5ce7f50a
Huy D. Vu <huyvd7@gmail.com>
2020-10-01 06:20:56 -0400

    Update proxgtv.py

--- proxgtv/proxgtv.py
+++ proxgtv/proxgtv.py
@@ -570,12 +570,12 @@ class GTV(nn.Module):
         # self.cnny.apply(weights_init_normal)
         self.cnnu.apply(weights_init_normal)
 
-        self.support_zmax = torch.ones(1).type(self.dtype)*0.01
-        self.support_identity = torch.eye(self.opt.width**2, self.opt.width**2).type(self.dtype)
-        self.support_L = torch.ones(opt.width**2, 1).type(self.dtype)
-        self.base_W = torch.zeros(self.opt.batch_size, self.opt.channels, self.opt.width ** 2, self.opt.width ** 2).type(self.dtype)
+        self.support_zmax = torch.ones(1).type(self.dtype).to('cuda')*0.01
+        self.support_identity = torch.eye(self.opt.width**2, self.opt.width**2).type(self.dtype.to('cuda'))
+        self.support_L = torch.ones(opt.width**2, 1).type(self.dtype.to('cuda'))
+        self.base_W = torch.zeros(self.opt.batch_size, self.opt.channels, self.opt.width ** 2, self.opt.width ** 2).type(self.dtype).to('cuda')
         self.lanczos_order = 20
-        self.support_e1 = torch.zeros(self.lanczos_order,1).type(self.dtype)
+        self.support_e1 = torch.zeros(self.lanczos_order,1).type(self.dtype).to('cuda')
         self.support_e1[0] = 1
     
     def forward(self, xf, debug=False, Tmod=False, manual_debug=False):  # gtvforward
