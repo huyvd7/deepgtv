e0b7678c258b6cfd6ad4dab14892df728c7d29f2
Huy D. Vu <huyvd7@gmail.com>
2020-10-03 01:50:25 -0400

    revert [0, 255]

--- proxgtv/proxgtv.py
+++ proxgtv/proxgtv.py
@@ -482,6 +482,7 @@ class OPT:
         self.ver = ver
         self.D = None
         self.train = train
+        self.pg_zero = None
         self.cuda= cuda
         if cuda:
             self.dtype = torch.cuda.FloatTensor
@@ -1171,7 +1172,20 @@ def supporting_matrix(opt):
 
     for e, p in enumerate(A_pair):
         A[p[1], p[0]] = 1
+    opt.lagrange = lagrange  # .requires_grad_(True)
+    opt.D = torch.inverse(2 * opt.I + opt.delta * (opt.H.T.mm(H))).type(dtype).detach()
+    opt.pg_zero = torch.zeros(opt.edges, 1).type(dtype)
     print("OPT created on cuda:", cuda, dtype)
+
+
+def proximal_gradient_descent(x, grad, w, u=1, eta=1, opt=None, debug=False):
+    v = x - eta * grad
+    # masks1 = ((v.abs() - (eta * w * u).abs()) > 0)#.type(dtype).requires_grad_(True)
+    # masks2 = ((v.abs() - (eta * w * u).abs()) <= 0)#.type(dtype).requires_grad_(True)
+    # v = v - masks1 * eta * w * u * torch.sign(v)
+    # v = v - masks2 * v
+    v = torch.sign(v) * torch.max(v.abs() - (eta * w * u), opt.pg_zero)
+    return v
 
 
 def _norm(x, newmin, newmax):
