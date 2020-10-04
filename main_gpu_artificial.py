1ac84c9f3f682f9fb28b9844d54dc4f3132567e1
Huy D. Vu <huyvd7@gmail.com>
2020-10-04 04:50:01 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -135,10 +135,13 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100, a
                 else:
                     print("\tCNNF stats: ", gtv.cnnf.layer1[0].weight.grad.mean().item())
                 print("\tCNNU grads: ", gtv.cnnu.layer[0].weight.grad.mean().item())
+                print("\tCNNU grads: ", gtv.cnns.layer[0].weight.grad.mean().item())
                 pmax = list()
 
                 with torch.no_grad():
                     us = gtv.cnnu(inputs)
+                    print("\tCNNU stats: ", us.max().item(),  us.mean().item(),us.min().item())
+                    us = gtv.cnns(inputs)
                     print("\tCNNU stats: ", us.max().item(),  us.mean().item(),us.min().item())
 
 
