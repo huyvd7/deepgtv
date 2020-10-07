5172c73a04df4c04f425ac98d196a25e58fc3c9c
Huy D. Vu <huyvd7@gmail.com>
2020-10-07 01:38:44 -0400

    Update main_gpu_artificial.py

--- main_gpu_artificial.py
+++ main_gpu_artificial.py
@@ -14,7 +14,7 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from proxgtv.proxgtv import * 
 import pickle
-logging.basicConfig(filename='evaluate_{0}.log'.format(time.strftime("%Y-%m-%d-%H%M")),
+logging.basicConfig(filename='main_gpu_artificial_{0}.log'.format(time.strftime("%Y-%m-%d-%H%M")),
                             filemode='a',
                             format='%(asctime)s %(name)s %(levelname)s %(message)s',
                             datefmt='%H:%M:%S',
