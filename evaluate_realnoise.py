e3c817cb7c3c9aad63ceb9428806076d3899061b
Huy D. Vu <huyvd7@gmail.com>
2020-10-07 01:00:09 -0400

    clean up unused files

--- evaluate_realnoise.py
+++
@@ -1,6 +0,0 @@
-from evaluate import *
-
-if __name__=="__main__":
-    global opt
-    supporting_matrix(opt)
-    _, _ = main_eva(seed='_', model_name='GTV.pkl', trainset=['10','1','7','8','9'], testset=['2','3','4','5','6'],imgw=324, verbose=1, image_path='..\\all\\all', noise_type='real')
