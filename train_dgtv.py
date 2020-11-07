8b10d1c99bee92a3a00a9ca88b87134cee441630
Huy D. Vu <huyvd7@gmail.com>
2020-11-06 22:06:32 -0500

    Update train_dgtv.py

--- train_dgtv.py
+++ train_dgtv.py
@@ -241,7 +241,6 @@ opt = OPT(
     u_min=50,
     cuda=True if torch.cuda.is_available() else False,
 )
-# batch_size = 50, admm_iter=4, prox_iter=3, delta=.1, channels=3, eta=.3, u=50, lr=8e-6, momentum=0.9, u_max=65, u_min=50)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -249,9 +248,6 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--cont", default=None)
     parser.add_argument("--batch", default=64, type=int)
     parser.add_argument("--lr", default=8e-6)
-    parser.add_argument("--delta", default=0.05)
-    parser.add_argument("--eta", default=0.05, type=float)
-    parser.add_argument("--admm_iter", default=4)
     parser.add_argument("--epoch", default=200, type=int)
     parser.add_argument("--umax", default=1000, type=float)
     parser.add_argument("--umin", default=0.001, type=float)
