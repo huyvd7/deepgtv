0401369dd3e9129e0c7923f73c45e4f756ca7088
Huy D. Vu <huyvd7@gmail.com>
2021-02-19 00:13:41 -0500

    Update train_dgtv.py

--- train_dgtv.py
+++ train_dgtv.py
@@ -134,15 +134,15 @@ def main(seed, model_name, cont=None, optim_name=None, subset=None, epoch=100):
                     )
                     P1 = g(inputs, debug=1)
                 P1 = g(inputs, debug=1)
+                print(g.cnnf.alphas1.grad)
+                #E = g.cnnf(inputs)
+                #Fs = (
+                #    g.opt.H.matmul(E.view(E.shape[0], E.shape[1], g.opt.width ** 2, 1))
+                #        ** 2
+                #)
 
-                E = g.cnnf(inputs)
-                Fs = (
-                    g.opt.H.matmul(E.view(E.shape[0], E.shape[1], g.opt.width ** 2, 1))
-                        ** 2
-                )
-
-                w = torch.exp(-(Fs.sum(axis=1)) / (g.weight_sigma ** 2))
-                print(E, Fs, w)
+                #w = torch.exp(-(Fs.sum(axis=1)) / (g.weight_sigma ** 2))
+                #print(E, Fs, w)
                 
                 #opt.logger.info(
                 #        "\tCNNF grads: {0:.5f}".format(
