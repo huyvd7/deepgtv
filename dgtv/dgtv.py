ff922d0a2c77787f6edb161bc9da9ed1b3ba38f6
Huy D. Vu <huyvd7@gmail.com>
2021-02-27 01:39:13 -0500

    Update dgtv.py

--- dgtv/dgtv.py
+++ dgtv/dgtv.py
@@ -54,7 +54,7 @@ class FNet(nn.Module):
             lowpfilter = np.exp(-((X - (shape1/2)) ** 2 + (Y - (shape0/2)) ** 2) / (2*Do*Do))
             f[:, i, :, :] = lowpfilter
         print(lowpfilter)
-        f = torch.from_numpy(f).type(dtype)
+        f = torch.from_numpy(f/1e2).type(dtype)
         self.base_fs.append(f)
         
     def forward(self, x):
