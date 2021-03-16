9e417e7b0e9adf582add94ca1aca2a798d6927a5
Huy D. Vu <huyvd7@gmail.com>
2021-03-16 00:09:17 -0400

    Update utils.py

--- legacy/utils.py
+++ legacy/utils.py
@@ -229,7 +229,8 @@ def _main(imgw=324, sigma=25, args=None):
     gaussp=args.path
     noisyp = os.path.join(gaussp, 'noisy')
     refp   = os.path.join(gaussp, 'ref')
-
+    noisyp_o = noisyp
+    refp_o = refp
     shutil.rmtree(gaussp, ignore_errors=True)
     shutil.rmtree(noisyp, ignore_errors=True)
     shutil.rmtree(refp, ignore_errors=True)
@@ -262,7 +263,7 @@ def _main(imgw=324, sigma=25, args=None):
     if args.noise!='gauss':
         args.path=args.train
         dataset = RENOIR_Dataset2(img_dir=args.path,
-                             transform = transforms.Compose([standardize2(w=imgw),
+                             transform = transforms.Compose([standardize2(),
                                                 ToTensor2()])
                             )
     else:
@@ -286,6 +287,10 @@ def _main(imgw=324, sigma=25, args=None):
     stride=args.stride
     psize=args.patch_size
     for i_batch, s in enumerate(dataloader):
+        if args.noise!='gauss':
+            # save noisy and ref images after resizing
+            plt.imsave(f'{noisyp_o}/{testset[i]}_n.bmp', img )
+            plt.imsave(f'{refp_o}/{testset[i]}_r.bmp', img )
         print(i_batch, s['nimg'].size(),
               s['rimg'].size(), len(s['nimg']), s['nn'], s['rn'])
         T1 = s['nimg'].unfold(2, psize, stride).unfold(3, psize, stride).reshape(1, 3, -1, psize, psize).squeeze()
